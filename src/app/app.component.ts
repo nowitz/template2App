54b40b4b00f6bee1466e227a2464b7448f0fda90
Jan Novak <nowitz.jan@gmail.com>
2017-02-07 23:12:56 +0100

    push notification for Android - https://www.youtube.com/watch?v=7yXtzhqT1uk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,61 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform} from 'ionic-angular';
+import {StatusBar, Splashscreen} from 'ionic-native';
+import {Push, PushToken} from '@ionic/cloud-angular'; //COMMENT nastaveni push notifikaci
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import {Page1} from '../pages/page1/page1';
+import {Page2} from '../pages/page2/page2';
 
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+    @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+    rootPage: any = Page1;
 
-  pages: Array<{title: string, component: any}>;
+    pages: Array<{title: string, component: any}>;
 
-  constructor(public platform: Platform) {
-    this.initializeApp();
+    constructor(public platform: Platform, public push: Push) {
+        this.initializeApp();
 
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
-    ];
+        // used for an example of ngFor and navigation
+        this.pages = [
+            {title: 'Page One', component: Page1},
+            {title: 'Page Two', component: Page2}
+        ];
 
-  }
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+            Splashscreen.hide();
+        });
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
-  }
+        this.pushNotification();
+
+    }
+
+    //COMMENT push notifikace
+    pushNotification():void {
+        this.push.register().then((t: PushToken) => {
+            return this.push.saveToken(t);
+        }).then((t: PushToken) => {
+            console.log('Token saved:', t.token);
+        });
+        this.push.rx.notification()
+            .subscribe((msg) => {
+                alert(msg.title + ': ' + msg.text);
+            });
+    }
+
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(page.component);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,27 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular'; //COMMENT nastaveni push notifikaci
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+
+const cloudSettings: CloudSettings = { //COMMENT nastaveni push notifikaci https://www.youtube.com/watch?v=7yXtzhqT1uk
+  'core': {
+    'app_id': 'c0bb3856' //odpovida ionic.config.json("app_id": "c0bb3856")
+  },
+  'push': {
+    'sender_id': '686514125389',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
+};
 
 @NgModule({
   declarations: [
@@ -11,7 +30,9 @@ import { Page2 } from '../pages/page2/page2';
     Page2
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings) //COMMENT nastaveni push notifikaci
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
